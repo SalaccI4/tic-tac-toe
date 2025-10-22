096bc23807ee8526a181b0774bce2ec5f136c5bd
SalaccI4 <66245600+SalaccI4@users.noreply.github.com>
2025-10-22 13:26:34 -0700

    Remove frontend temporarily and implement Gameboard logic for board state

--- javascript.js
+++ javascript.js
@@ -1,45 +1,102 @@
 function Gameboard(){
-    
-    const container = document.querySelector(".container")
 
-    container.addEventListener("click", (e) => {
-        if (e.target.className === "cell") {
-            e.target.style.backgroundColor = "red";
-        }
-    })
-    
     let board = []
     const row = 3
     const col = 3
-
-    const implementCell = () => {
-        let cell = document.createElement("div")
-        cell.classList.add("cell")
-        container.append(cell)
-    }
 
     for (i = 0; i < row; i++){
         board[i] = []
         for (j = 0; j < col; j++) {
             board[i].push(Cell())
-            implementCell()
         }
     }
+
+    const getBoard = () => board
+
+    const createValueMap = () => {
+        const whoa = board.map(
+            (cellRow) => cellRow.map(
+                (cell) => cell.getValue()
+            )
+        )
+    }
+
+    const changeTileValue = (cellRow, cellCol, tile) => {
+        board[cellRow][cellCol].changeValue(tile)
+    }
+
+    const printValueMap = () => {
+        const whoa = board.map(
+            (cellRow) => cellRow.map(
+                (cell) => console.log(cell.getValue())
+            )
+        )
+    }
+
+
+    return{
+        getBoard,
+        createValueMap,
+        changeTileValue,
+        printValueMap
+    }
+
+    // const container = document.querySelector(".container")
+
+    // container.addEventListener("click", (e) => {
+    //     console.log(e.target.classList)
+    //     if (e.target.classList.contains("cell")) {
+    //         e.target.style.backgroundColor = "red";
+    //         console.log()
+    //     }
+    // })
     
+    // let board = []
+    // const row = 3
+    // const col = 3
 
-    // const findEmptyTile = board
-    //     .filter((row) => row[column].getValue == 0)
-    //     .map((row) => row[column])
+    // const implementCell = (rowNum, colNum) => {
+    //     let cell = document.createElement("div")
+    //     cell.classList.add("cell", `r${rowNum}`, `c${colNum}`)
+    //     container.append(cell)
+    // }
 
+    // for (i = 0; i < row; i++){
+    //     board[i] = []
+    //     for (j = 0; j < col; j++) {
+    //         implementCell(i, j)
+    //     }
+    // }
+    // console.log(board)
 }
 
 function Cell(){
     let value = 0
+    const changeValue = (changed) => value = changed
     const getValue = () => value
-
     return{
-        getValue
+        getValue, changeValue
     }
 }
 
+function controlGameFlow(){
+    //create player objects, 
+    const player1 = {
+        name: "", 
+        value: 1
+    }
+    const player2 = {
+        name: "",
+        value: 2
+    }
+
+
+}
+
+function controlDisplay(){
+    
+}
+
 const thing = Gameboard()
+thing.changeTileValue(0, 2, 2)
+thing.printValueMap()
